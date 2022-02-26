869009cef4f87cc5b45ac7862a4375811386a569
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2022-02-27 00:42:45 +0530

    Added login.dart and themes.dart

---
+++ lib/screens/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:fishies_sih/utils/colors.dart' as colors;
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
