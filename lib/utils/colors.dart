d00f544fcb35d1650746e7f1f00573960d70e609
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2022-02-27 00:14:47 +0530

    Added all colors to colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+var scaffoldColor = const Color(0xff1A1A2A);
+var primaryTextColor = const Color(0xffffffff);
+var textBoxColor = const Color(0xff2D2D41);
+var buttonColor = const Color(0xffffffff);
+var cardColor = const Color(0xffAEA9A9);
+var yesButtonColor = const Color(0xff49E79B);
+var noButtonColor = const Color(0xffE73D3D);
+var endButtonColor = const Color(0xffF54D4D);
